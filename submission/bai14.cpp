aeadf6d450bfb594b3941148c4353f80ab94d824
Tris Lee <quangtri2002lk@gmail.com>
2023-01-02 22:31:49 +0700

    naive approach

--- submission/bai14.cpp
+++ submission/bai14.cpp
@@ -28,37 +28,58 @@ struct Point
     }
 };
 
-double slope(Point p1, Point p2)
-{
-    double dx = p2.x - p1.x;
-    double dy = p2.y - p1.y;
 
-    if (dx == 0) { 
-        return INT_MAX;
-    }
-
-    return dy/(dx);
+double calcArea(vector<Point> points, int start, int end) {
+	double a = 0;
+	for (int i = start; i < end; ++i) {
+		a += (points[i+1].x - points[i].x)*(points[i+1].y + points[i].y);
+	}
+	a += (points[start].x - points[end].x) * (points[start].y + points[end].y);
+	return -a * 0.5;
 }
 
-
+double calcTriangleArea(Point a, Point b, Point c) {
+	return -0.5 * ((b.x - a.x)*(b.y + a.y) + (c.x - b.x)*(c.y + b.y) + (a.x - c.x)*(a.y + c.y));
+}
 
 // O(n)
 int approach1() {
-    int n, a, b;
-    cin >> n;	 
-    
-	cout << "IMPOSSIBLE";
+    int n, x, y, p1, p2;
+    double sij, s, areaDiff, min = INT_MAX;
+    vector<Point> points;
+    cin >> n;
+	
+	if (n < 4) return 1;
+		 
+    for (int i = 0; i < n; ++i) {
+    	cin >> x >> y;
+    	points.push_back(Point(x, y));
+	}
+	
+	s = calcArea(points, 0, points.size()-1);
+
+	for (int i = 0; i < n; ++i) {
+		sij = 0;
+		for (int j = 2; j < n-1; ++j) {
+			sij += calcTriangleArea(points[i], points[(i+j-1) % n], points[(i+j) % n]);
+			areaDiff = abs(2*sij - s);
+			if (areaDiff < min) {
+				min = areaDiff;
+				p1 = i;
+				p2 = i+j;
+			}
+		}
+		
+	}
+	
+	cout << p1+1 << " " << p2+1;
 }
 
 int main() {
-	freopen("input/bai13.in", "r",stdin);
-//  freopen("output/bai13.out", "w",stdout);
+	freopen("input/bai14.in", "r",stdin);
+//  freopen("output/bai14.out", "w",stdout);
     
-	double param, result;
-	param = INT_MAX;
-	result = atan(param) * 180 / PI;
-	
-	cout << result;
+	approach1();
 	
 	return 0;
 }
