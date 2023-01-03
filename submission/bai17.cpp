33b74e1c055354bc9ae884c4bec5add6b8fe2b5d
galin-chung-nguyen <galin.chung.nguyen@gmail.com>
2023-01-04 05:10:59 +0700

    review code

--- submission/bai17.cpp
+++ submission/bai17.cpp
@@ -5,34 +5,42 @@
 #include <cmath>
 using namespace std;
 
-unsigned int _try(unsigned int factor, unsigned int m, int n) {
-	if (factor == 0) {
-		if (m > 0 && m % n == 0) {
-			return m;
-		}
-		return -1;
-	} else {
-		unsigned int x = _try(factor/10, m, n);
-		if (x == -1) {
-			return _try(factor/10, factor + m, n);
-		}
-		return x;
-	}
+long long _try(long long factor, long long m, int n)
+{
+  if (factor == 0)
+  {
+    if (m > 0 && m % n == 0)
+    {
+      return m;
+    }
+    return -1; // FIXED: unsigned int -> long long to hold value -1
+  }
+  else
+  {
+    long long x = _try(factor / 10, m, n);
+    if (x == -1)
+    {
+      return _try(factor / 10, factor + m, n);
+    }
+    return x;
+  }
 }
 
 // O(n)
-int approach1() {
-    int n;
-    unsigned int m = 0;
-    cin >> n;
-    cout << _try(100000000, m, n);
+void approach1()
+{
+  int n;
+  long long m = 0;
+  cin >> n;
+  cout << _try(100000000, m, n);
 }
 
-int main() {
-	freopen("input/bai17.in", "r",stdin);
-//  freopen("output/bai17.out", "w",stdout);
-    
-	approach1();
-	
-	return 0;
+int main()
+{
+  freopen("input/bai17.in", "r", stdin);
+  //  freopen("output/bai17.out", "w",stdout);
+
+  approach1();
+
+  return 0;
 }
